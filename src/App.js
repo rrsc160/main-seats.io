1c8e784f8b938096afc64e062e067e783c2456c3
rrsc160 <rrsc160@gmail.com>
2025-03-24 14:56:03 +0530

    Update App.js
    added loader

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import React from "react";
-import SeatsChart from "./components/SeatsChart";
+
+import React, { lazy, Suspense } from "react";
 import "./style/style.css";
+
+const SeatsChart = lazy(() => import("./components/SeatsChart"));
 
 const App = () => {
   return (
     <div className="App">
-      <SeatsChart />
+      <Suspense
+        fallback={
+          <div className="loading-container">
+            <div className="chart-placeholder"></div>
+            <div className="loading-text">Loading...</div>
+          </div>
+        }
+      >
+        <SeatsChart />
+      </Suspense>
     </div>
   );
 };
